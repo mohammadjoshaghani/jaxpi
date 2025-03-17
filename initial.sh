eabcaf711c0ed6b20d2486312b6d6d59a950ff42
Mohammad Joshaghani <mjo@cedar5.int.cedar.computecanada.ca>
2025-03-17 15:03:32 -0700

    update

--- initial.sh
+++ initial.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-module load python/3.13.2
-virtualenv --no-download pinns
+cd ~
+module load python/3.10
+virtualenv pinns
 source ~/pinns/bin/activate
-pip install -r /projects/def-jenasanj/mjo/jaxpi/requirements.text 
+pip install -r ~/projects/def-jenasanj/mjo/jaxpi/requirements.text 
