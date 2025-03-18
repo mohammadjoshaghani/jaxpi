8c1dc8d1600f7091a61cff0c40a87b36277d871a
Mohammad Joshaghani <mjoshaghani10@gmail.com>
2025-03-17 20:08:07 -0400

    modify KS for saving

--- examples/ks_chaotic/train.py
+++ examples/ks_chaotic/train.py
@@ -58,7 +58,7 @@ def train_one_window(config, workdir, model, res_sampler, u_ref, idx):
             if (step + 1) % config.saving.save_every_steps == 0 or (
                 step + 1
             ) == config.training.max_steps:
-                ckpt_path = os.path.join(os.getcwd(), config.wandb.name, "ckpt", "time_window_{}".format(idx + 1))
+                ckpt_path = os.path.join(os.getcwd(), config.wandb.name, config.optim.optimizer ,"ckpt", "time_window_{}".format(idx + 1))
                 save_checkpoint(model.state, ckpt_path, keep=config.saving.num_keep_ckpts)
 
     return model
