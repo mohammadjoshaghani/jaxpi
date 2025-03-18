a1ca4328fcf5a84567a947b8a54d8dfe662a87c1
Mohammad Joshaghani <mjo@cedar5.int.cedar.computecanada.ca>
2025-03-17 17:12:35 -0700

    python 3.10

--- run.sh
+++ run.sh
@@ -7,9 +7,8 @@
 #SBATCH --mail-user=<mjoshaghani10@gmail.com>
 #SBATCH --mail-type=ALL
 
-module load python/3.13.2
+module load python/3.10
 source ~/pinns/bin/activate
-wandb login fcd0350810adb4e53c9880f13cdc0d106327d04d
 git checkout pirate
 cd ~/projects/def-jenasanj/mjo/jaxpi/examples/burgers
 python main.py
